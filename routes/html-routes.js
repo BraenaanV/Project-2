4d9dfffce3f7b791f977b74a828dec055b4faa67
Matthew Pollicito <mpollicito@yahoo.com>
2020-11-11 20:16:01 -0500

    Landing page for registered user, tasks to be displayed here

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -21,6 +21,22 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
+  app.get("/tasks", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/tasks");
+    }
+    res.sendFile(path.join(__dirname, "../public/tasks.html"));
+  });
+
+  app.get("/tasks2", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/tasks2");
+    }
+    res.sendFile(path.join(__dirname, "../public/tasks2.html"));
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
